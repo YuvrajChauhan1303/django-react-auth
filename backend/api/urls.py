f2b3a29caa828163c67656d6b8197bf20b7a265b
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-06-28 17:39:13 +0530

    backend completed

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -16,16 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateUserView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+from . import views
 
 urlpatterns = [
-    
-    path('api/token/', TokenObtainPairView.as_view(), name="get_token"),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name="refresh"),
-    path('api-auth/', include("rest_framework.urls")),
-
-    path('api/user/register/' , CreateUserView.as_view(), name="register"),
-
-    path("api/", include("api.urls")) 
+    path('notes/', views.CreateNewNote.as_view(), name="note-list"),
+    path('notes/delete/<int:pk>/', views.NoteDelete.as_view(), name="note-del"),
 ]
